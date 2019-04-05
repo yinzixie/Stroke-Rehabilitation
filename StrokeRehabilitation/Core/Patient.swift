0e058d0dcbac27bf0ccf9aad75017b9d32149a77
yinzixie <yinzixie@yinzixies-Mac.local>
2019-04-04 23:52:53 -0700

    test1

---
+++ StrokeRehabilitation/Core/Patient.swift
@@ -0,0 +1,29 @@
+//
+//  Patient.swift
+//  StrokeRehabilitation
+//
+//  Created by yinzixie on 4/4/19.
+//  Copyright © 2019 Project. All rights reserved.
+//
+
+import Foundation
+
+class Patient {
+    var test
+    enum gender {
+        case man
+        case women
+        case neutral
+    }
+    
+    var ID:Int
+    var Gender:gender
+    var AimMissionList = [Mission]()
+    var HistoryMissionList = [Mission]()
+    
+    init(id:Int,sex:gender) {
+        ID = id
+        Gender = sex
+    }
+    
+}
---
+++ StrokeRehabilitation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StrokeRehabilitation
+//
+//  Created by yinzixie on 2/4/19.
+//  Copyright © 2019 Project. All rights reserved.
+//
+
+import UIKit
+var test
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
